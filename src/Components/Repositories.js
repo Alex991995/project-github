ddb06d4f6c10997bb185eea73c034412bece578f
Alex991995 <51911663+Alex991995@users.noreply.github.com>
2023-09-19 19:04:55 +0300

    fix: some layot and adaptive

--- src/Components/Repositories.js
+++ src/Components/Repositories.js
@@ -13,14 +13,14 @@ function Repositories() {
       .then(data =>setRepos(data))
   },[user])
 
+  
   return (
     <section>
-        <ul className="grid grid-cols-4 gap-4 justify-center my-4">
+        <ul className="grid sm:grid-cols-2 gap-4 justify-center m-4 md:grid-cols-3">
         {repos?.map(item => (
           <li key={item.id} className="flex flex-col p-4 border-zinc-500 border  hover:shadow-md "> 
           <span className="italic">Name: {item.name}</span>
-          {/* <span>{item.language ? "language:"`${item.language}` : "No language"}</span> */}
-          <span className="font-bold">{item.language && "language: " + item.language }</span>
+          <span className="font-bold">{"language: "+ item.language ?? "No language "}</span>
           <span className="font-['Helvetica']">{item.description && "Description: " + item.description}</span>
           <span className="underline font-arial hover:no-underline "><a href={item.html_url}>Link</a></span>
           </li>
